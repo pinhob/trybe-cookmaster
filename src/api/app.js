6a6c8214a29cbe075ef8cc0a161506be1f897572
Bruno Pinho <brunopinho@outlook.com>
2022-01-22 12:54:34 -0300

    criada estrutura inicial do projeto

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// necessário para o projeto, de acordo com o readme
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador!
 app.get('/', (request, response) => {
